815ab4d900f45007775e1b3165d9f6ac68becdac
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-28 18:24:57 -0600

    init commit

---
+++ objects.js
@@ -0,0 +1,14 @@
+let exampleObject = {
+    name: "Dante",
+    age: 42,
+    city: "New York"
+}
+
+// log the object
+console.log(exampleObject);
+
+// log the object's name
+console.log(exampleObject.name);
+
+// log the object's age and city
+console.log(exampleObject.age, exampleObject.city);
